4dbf960628b7034eac6f812f7add4233ac9ce9ae
AlisonS <alison.shaw@maidsafe.net>
2012-12-17 15:46:57 +0000

    [#24431734] Updated and added more message tests; improved style.

--- src/maidsafe/nfs/tests/message_test.cc
+++ src/maidsafe/nfs/tests/message_test.cc
@@ -26,7 +26,7 @@ ActionType GenerateActionType() {
 }
 
 PersonaType GeneratePersonaType() {
-  return static_cast<PersonaType>(RandomUint32() % 4); // matches enum in message.h
+  return static_cast<PersonaType>(RandomUint32() % 4);  // matches enum in message.h
 }
 
 class MessageTest : public testing::Test {
@@ -61,8 +61,8 @@ TEST_F(MessageTest, FUNC_CheckGetters) {
                   content_,
                   signature_);
   EXPECT_EQ(action_type_, message.action_type());
-  EXPECT_EQ(src_persona_type_, message.source_persona_type());
   EXPECT_EQ(dest_persona_type_, message.destination_persona_type());
+  EXPECT_EQ(src_persona_type_, message.source_persona_type());
   EXPECT_EQ(data_type_, message.data_type());
   EXPECT_EQ(dest_id_, message.destination());
   EXPECT_EQ(src_id_, message.source());
@@ -72,12 +72,20 @@ TEST_F(MessageTest, FUNC_CheckGetters) {
 
 /*
 TEST_F(MessageTest, FUNC_SerialiseThenParse) {
-  Message message(action_type_, persona_type_, data_type_, dest_id_, src_id_, content_, signature_);
+  Message message(action_type_,
+                  dest_persona_type_,
+                  src_persona_type_,
+                  data_type_,
+                  dest_id_,
+                  src_id_,
+                  content_,
+                  signature_);
   std::string string(SerialiseAsString(message));
   Message message2(ParseFromString(string));
 
   EXPECT_EQ(action_type_, message2.action_type());
-  EXPECT_EQ(persona_type_, message2.persona_type());
+  EXPECT_EQ(dest_persona_type_, message2.destination_persona_type());
+  EXPECT_EQ(src_persona_type_, message2.source_persona_type());
   EXPECT_EQ(data_type_, message2.data_type());
   EXPECT_EQ(dest_id_, message2.destination());
   EXPECT_EQ(src_id_, message2.source());
@@ -86,7 +94,14 @@ TEST_F(MessageTest, FUNC_SerialiseThenParse) {
 }
 
 TEST_F(MessageTest, FUNC_ParseFromBadString) {
-  Message message(action_type_, persona_type_, data_type_, dest_id_, src_id_, content_, signature_);
+  Message message(action_type_,
+                  dest_persona_type_,
+                  src_persona_type_,
+                  data_type_,
+                  dest_id_,
+                  src_id_,
+                  content_,
+                  signature_);
   std::vector<std::string> bad_strings;
   bad_strings.push_back(RandomAlphaNumericString(1 + RandomUint32() % 100));
   bad_strings.push_back("");
@@ -106,12 +121,20 @@ TEST_F(MessageTest, FUNC_ParseFromBadString) {
 }
 
 TEST_F(MessageTest, FUNC_SerialiseParseReserialiseReparse) {
-  Message message(action_type_, persona_type_, data_type_, dest_id_, src_id_, content_, signature_);
+  Message message(action_type_,
+                  dest_persona_type_,
+                  src_persona_type_,
+                  data_type_,
+                  dest_id_,
+                  src_id_,
+                  content_,
+                  signature_);
   std::string string(SerialiseAsString(message));
   Message message2(ParseFromString(string));
 
   EXPECT_EQ(action_type_, message2.action_type());
-  EXPECT_EQ(persona_type_, message2.persona_type());
+  EXPECT_EQ(dest_persona_type_, message2.destination_persona_type());
+  EXPECT_EQ(src_persona_type_, message2.source_persona_type());
   EXPECT_EQ(data_type_, message2.data_type());
   EXPECT_EQ(dest_id_, message2.destination());
   EXPECT_EQ(src_id_, message2.source());
@@ -122,7 +145,8 @@ TEST_F(MessageTest, FUNC_SerialiseParseReserialiseReparse) {
   Message message3(ParseFromString(string2));
 
   EXPECT_EQ(action_type_, message3.action_type());
-  EXPECT_EQ(persona_type_, message3.persona_type());
+  EXPECT_EQ(dest_persona_type_, message3.destination_persona_type());
+  EXPECT_EQ(src_persona_type_, message3.source_persona_type());
   EXPECT_EQ(data_type_, message3.data_type());
   EXPECT_EQ(dest_id_, message3.destination());
   EXPECT_EQ(src_id_, message3.source());
@@ -131,11 +155,19 @@ TEST_F(MessageTest, FUNC_SerialiseParseReserialiseReparse) {
 }
 
 TEST_F(MessageTest, FUNC_AssignMessage) {
-  Message message(action_type_, persona_type_, data_type_, dest_id_, src_id_, content_, signature_);
+  Message message(action_type_,
+                  dest_persona_type_,
+                  src_persona_type_,
+                  data_type_,
+                  dest_id_,
+                  src_id_,
+                  content_,
+                  signature_);
   Message message2 = message;
 
   EXPECT_EQ(action_type_, message2.action_type());
-  EXPECT_EQ(persona_type_, message2.persona_type());
+  EXPECT_EQ(dest_persona_type_, message2.destination_persona_type());
+  EXPECT_EQ(src_persona_type_, message2.source_persona_type());
   EXPECT_EQ(data_type_, message2.data_type());
   EXPECT_EQ(dest_id_, message2.destination());
   EXPECT_EQ(src_id_, message2.source());
@@ -145,7 +177,8 @@ TEST_F(MessageTest, FUNC_AssignMessage) {
   Message message3(message);
 
   EXPECT_EQ(action_type_, message3.action_type());
-  EXPECT_EQ(persona_type_, message3.persona_type());
+  EXPECT_EQ(dest_persona_type_, message3.destination_persona_type());
+  EXPECT_EQ(src_persona_type_, message3.source_persona_type());
   EXPECT_EQ(data_type_, message3.data_type());
   EXPECT_EQ(dest_id_, message3.destination());
   EXPECT_EQ(src_id_, message3.source());
@@ -153,36 +186,61 @@ TEST_F(MessageTest, FUNC_AssignMessage) {
   EXPECT_EQ(signature_, message3.signature());
 }
 
+
 TEST_F(MessageTest, BadDataType) {
-  // TODO(Alison)
   int32_t bad_data_type(RandomInt32());
   while (bad_data_type >= 0)
     bad_data_type = RandomInt32();
   try {
     Message message(action_type_,
-                    persona_type_,
+                    dest_persona_type_,
+                    src_persona_type_,
                     bad_data_type,
                     dest_id_,
                     src_id_,
                     content_,
                     signature_);
-    ASSERT_TRUE(false) <<;
+    ASSERT_TRUE(false);
   } catch (const std::exception& ex) {
-
+    LOG(kInfo) << "Threw on bad data type: " << ex.what();
   }
 }
 
 TEST_F(MessageTest, BadSource) {
-  // TODO(Alison)
+  try {
+    Message message(action_type_,
+                    dest_persona_type_,
+                    src_persona_type_,
+                    data_type_,
+                    dest_id_,
+                    NodeId(),
+                    content_,
+                    signature_);
+    ASSERT_TRUE(false);
+  } catch (const std::exception& ex) {
+    LOG(kInfo) << "Threw on bad source: " << ex.what();
+  }
 }
 
 TEST_F(MessageTest, BadDestination) {
-  // TODO(Alison)
+  try {
+    Message message(action_type_,
+                    dest_persona_type_,
+                    src_persona_type_,
+                    data_type_,
+                    NodeId(),
+                    src_id_,
+                    content_,
+                    signature_);
+    ASSERT_TRUE(false);
+  } catch (const std::exception& ex) {
+    LOG(kInfo) << "Threw on bad destination: " << ex.what();
+  }
 }
 */
 
-}
+}  // namespace test
 
-}
+}  // namespace nfs
 
-}
+}  // namespace maidsafe
