ba072ac0ef65b0f50c31ad9a415b550112c815d7
David Irvine <david.irvine@maidsafe.net>
2012-12-26 09:43:26 +0000

    update to compile clang/msvc

--- include/maidsafe/nfs/utils.h
+++ include/maidsafe/nfs/utils.h
@@ -90,11 +90,10 @@ void HandlePutResponse(std::function<void(Message message)> on_error_functor,
 
   // TODO(Fraser#5#): 2012-12-21 - Confirm this is OK as a means of deciding overall success
   int success_count(0), failure_count(0);
-  ReturnCode return_code;
   for (auto& serialised_message : serialised_messages) {
     try {
       // Need '((' when constructing ReturnCode to avoid most vexing parse.
-      return_code = ReturnCode((ReturnCode::serialised_type((NonEmptyString(serialised_message)))));
+      ReturnCode return_code((ReturnCode::serialised_type((NonEmptyString(serialised_message)))));
       if (static_cast<CommonErrors>(return_code.value()) == CommonErrors::success) {
         ++success_count;
       } else {
@@ -106,7 +105,7 @@ void HandlePutResponse(std::function<void(Message message)> on_error_functor,
     }
     catch(const std::exception& e) {
       ++failure_count;
-      LOG(kError) << return_code.value() << " - " << e.what();
+      LOG(kError) <<  e.what();
     }
   }
 
