304c598ff289cdc97b3ee3355118084f6bcdd359
anju.bala <anju.bala@maidsafe.net>
2013-01-30 14:59:59 +0000

    Added test case for handling exception[#28757260]

--- src/maidsafe/nfs/tests/response_mapper_test.cc
+++ src/maidsafe/nfs/tests/response_mapper_test.cc
@@ -45,6 +45,15 @@ class ResponseMapperTest : public testing::Test {
     promise2.erase(promise2.begin() + index);
   }
 
+  void SetExceptionPromise(std::vector<std::promise<std::string>> &promise2) {
+    if (promise2.empty())
+      return;
+    uint32_t index = (RandomUint32() % promise2.size());
+    promise2.at(index).set_exception(std::make_exception_ptr(
+        MakeError(NfsErrors::failed_to_get_data)));
+    promise2.erase(promise2.begin() + index);
+  }
+
   bool CheckOutput(std::vector<std::string> &inputs, ReturnCode::serialised_type output) {
     return (std::find_if(inputs.begin(), inputs.end(),
                          [output] (const std::string& element) {
@@ -94,6 +103,51 @@ TEST_F(ResponseMapperTest, BEH_push_back) {
     }
     if (future1.empty())
       done = false;
+  }
+}
+
+TEST_F(ResponseMapperTest, BEH_push_back_With_Exception) {
+  uint16_t num_inputs(1);
+  std::vector<std::string> inputs;
+  std::vector<std::promise<ReturnCode>> promise1(num_inputs);
+  std::vector<std::promise<std::string>> promise2(num_inputs);
+  std::vector<std::future<ReturnCode>> future1;
+  auto promise2_itr = promise2.begin();
+  auto promise1_itr = promise1.begin();
+  while (!promise1.empty()) {
+    inputs.push_back(RandomAlphaNumericString(16));
+    future1.emplace_back((*promise1_itr).get_future());
+    std::future<std::string> future2 = (*promise2_itr).get_future();
+    std::promise<ReturnCode> promise(std::move(*promise1_itr));
+    FuturePromisePair pair(std::make_pair(std::move(future2), std::move(promise)));
+    response_mapper_.push_back(std::move(pair));
+    promise1.erase(promise1_itr);
+  }
+  //  Set promises values
+  while (!promise2.empty())
+    SetExceptionPromise(promise2);
+
+  auto future_itr = future1.begin();
+  bool done(true);
+  while (done) {
+    auto future_itr = future1.begin();
+    while (future_itr != future1.end()) {
+      try {
+        if (IsReady(*future_itr)) {
+          ReturnCode return_code = (*future_itr).get();
+          ReturnCode::serialised_type serialised_resp = return_code.Serialise();
+          EXPECT_TRUE(CheckOutput(inputs, serialised_resp));
+          future_itr = future1.erase(future_itr);
+        } else {
+          ++future_itr;
+        }
+      }
+      catch(const NfsErrors &error) {
+        EXPECT_EQ(error, NfsErrors::failed_to_get_data);
+      }
+      if (future1.empty())
+        done = false;
+    }
   }
 }
 
