5cc11baa45c5708b64077b8c6bfd01c696157e7c
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2013-02-11 14:09:05 +0000

    Added convenience functions wrapping client NFS Put, Get and Delete calls.

--- include/maidsafe/nfs/client_utils.h
+++ include/maidsafe/nfs/client_utils.h
@@ -12,16 +12,12 @@
 #ifndef MAIDSAFE_NFS_CLIENT_UTILS_H_
 #define MAIDSAFE_NFS_CLIENT_UTILS_H_
 
-#include <algorithm>
-#include <exception>
 #include <future>
-#include <map>
 #include <mutex>
 #include <string>
 #include <vector>
 
 #include "maidsafe/common/error.h"
-#include "maidsafe/common/log.h"
 
 #include "maidsafe/nfs/reply.h"
 #include "maidsafe/nfs/nfs.h"
@@ -33,42 +29,34 @@ namespace nfs {
 
 namespace detail {
 
+class PutDeleteOp {
+ public:
+  PutDeleteOp(int successes_required, std::function<void(Reply)> callback);
+  void HandleReply(Reply&& reply);
+
+ private:
+  PutDeleteOp(const PutDeleteOp&);
+  PutDeleteOp& operator=(const PutDeleteOp&);
+  PutDeleteOp(PutDeleteOp&&);
+  PutDeleteOp& operator=(PutDeleteOp&&);
+
+  mutable std::mutex mutex_;
+  int successes_required_;
+  std::function<void(Reply)> callback_;
+  std::vector<Reply> replies_;
+  bool callback_executed_;
+};
+
+void HandlePutOrDeleteReply(std::shared_ptr<detail::PutDeleteOp> op,
+                            const std::string& serialised_reply);
+
 template<typename Data>
 class GetOp {
  public:
-  GetOp() : promise_(), errors_(), mutex_(), promise_set_(false) {}
-
+  GetOp() : mutex_(), promise_(), promise_set_(false), errors_() {}
   std::future<Data> GetFutureFromPromise() { return promise_.get_future(); }
-
-  void SetPromiseValue(Data&& data) {
-    std::lock_guard<std::mutex> lock(mutex_);
-    if (promise_set_)
-      return;
-    promise_set_ = true;
-    promise_.set_value(data);
-  }
-
-  void HandleFailure(const maidsafe_error& error) {
-    std::lock_guard<std::mutex> lock(mutex_);
-    if (promise_set_)
-      return;
-    errors_.push_back(error);
-    if (errors_.size() == 4) {
-      // Operation has failed, get most frequent error_code.
-      promise_set_ = true;
-      typedef std::map<std::error_code, int> Count;
-      Count count;
-      for (auto& error : errors_)
-        ++count[error.code()];
-      auto itr(std::max_element(
-          count.begin(),
-          count.end(),
-          [](const Count::value_type& lhs, const Count::value_type& rhs)->bool {
-              return lhs.second < rhs.second;
-          }));
-      promise_.set_exception(std::make_exception_ptr((*itr).first));
-    }
-  }
+  void SetPromiseValue(Data&& data);
+  void HandleFailure(const maidsafe_error& error);
 
  private:
   GetOp(const GetOp&);
@@ -76,39 +64,33 @@ class GetOp {
   GetOp(GetOp&&);
   GetOp& operator=(GetOp&&);
 
-  std::promise<Data> promise_;
-  std::vector<maidsafe_error> errors_;
   mutable std::mutex mutex_;
+  std::promise<Data> promise_;
   bool promise_set_;
+  std::vector<maidsafe_error> errors_;
 };
 
 }  // namespace detail
 
 template<typename Data>
-std::future<Data> Get(ClientMaidNfs& client_maid_nfs, const typename Data::name_type& name) {
-  std::shared_ptr<detail::GetOp<Data>> get_op;
-  client_maid_nfs.Get(name, [get_op](std::string serialised_reply)->void {
-    try {
-      Reply reply((Reply::serialised_type(NonEmptyString(serialised_reply))));
-      if (!reply.IsSuccess())
-        throw reply.error();
-      Data data((Data::serialised_type(reply.data())));
-      return get_op->SetPromiseValue(std::move(data));
-    }
-    catch(const maidsafe_error& error) {
-      LOG(kWarning) << "nfs Get error: " << error.code() << " - " << error.what();
-      get_op->HandleFailure(error);
-    }
-    catch(const std::exception& e) {
-      LOG(kWarning) << "nfs Get error: " << e.what();
-      get_op->HandleFailure(CommonErrors::unknown);
-    }
-  });
-  return get_op->GetFutureFromPromise();
-}
+void Put(ClientMaidNfs& client_maid_nfs,
+         const Data& data,
+         int successes_required,
+         std::function<void(Reply)> result);
+
+template<typename Data>
+std::future<Data> Get(ClientMaidNfs& client_maid_nfs, const typename Data::name_type& name);
+
+template<typename Data>
+void Delete(ClientMaidNfs& client_maid_nfs,
+            const typename Data::name_type& name,
+            int successes_required,
+            std::function<void(Reply)> result);
 
 }  // namespace nfs
 
 }  // namespace maidsafe
+
+#include "maidsafe/nfs/client_utils-inl.h"
 
 #endif  // MAIDSAFE_NFS_CLIENT_UTILS_H_
