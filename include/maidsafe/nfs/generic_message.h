568c21044080a331b85da0999c46a7584d2402d1
David Irvine <david.irvine@maidsafe.net>
2013-03-03 01:45:09 +0000

    Update for pmid health status

--- include/maidsafe/nfs/generic_message.h
+++ include/maidsafe/nfs/generic_message.h
@@ -20,7 +20,6 @@
 #include "maidsafe/common/rsa.h"
 #include "maidsafe/common/types.h"
 #include "maidsafe/data_types/data_type_values.h"
-
 #include "maidsafe/nfs/persona_id.h"
 #include "maidsafe/nfs/types.h"
 
@@ -34,9 +33,10 @@ class GenericMessage {
   enum class Action : int32_t {
     kRegisterPmid,
     kConnect,
-    kGetPmidSize,
+    kGetPmidHealth,
     kNodeDown,
     kNodeUp,
+    kPmidStatus,
     kGetElementList,
     kSynchronise
   };
@@ -87,7 +87,6 @@ class GenericMessage {
   NonEmptyString content_;
 };
 
-
 template <typename Elem, typename Traits>
 std::basic_ostream<Elem, Traits>& operator<<(std::basic_ostream<Elem, Traits>& ostream,
                                              const GenericMessage::Action &action) {
@@ -99,7 +98,7 @@ std::basic_ostream<Elem, Traits>& operator<<(std::basic_ostream<Elem, Traits>& o
     case GenericMessage::Action::kConnect:
       action_str = "Connect";
       break;
-    case GenericMessage::Action::kGetPmidSize:
+    case GenericMessage::Action::kGetPmidHealth:
       action_str = "Get PMID Size";
       break;
     case GenericMessage::Action::kNodeDown:
