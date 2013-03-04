158dba303cae3f72a9331fc855f27a7a3962d16b
Dan Schmidt <dan.schmidt@maidsafe.net>
2013-03-04 18:11:27 +0000

    Moved non-template function to client_utils.cc [#32714049]

--- include/maidsafe/nfs/client_utils-inl.h
+++ include/maidsafe/nfs/client_utils-inl.h
@@ -36,19 +36,7 @@ namespace nfs {
 
 namespace detail {
 
-std::error_code GetMostFrequentError(const std::vector<maidsafe_error>& errors) {
-  typedef std::map<std::error_code, int> Count;
-  Count count;
-  for (auto& error : errors)
-    ++count[error.code()];
-  auto itr(std::max_element(
-      count.begin(),
-      count.end(),
-      [](const Count::value_type& lhs, const Count::value_type& rhs)->bool {
-          return lhs.second < rhs.second;
-      }));
-  return (*itr).first;
-}
+std::error_code GetMostFrequentError(const std::vector<maidsafe_error>& errors);
 
 template<typename Data>
 void GetOp<Data>::SetPromiseValue(Data&& data) {
