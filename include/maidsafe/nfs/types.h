2a798d5e8da36aeb02d3dfe5fca798dcf1e48b08
David Irvine <david.irvine@maidsafe.net>
2013-05-31 10:10:27 +0100

    Reflect changes to key value types per persona

--- include/maidsafe/nfs/types.h
+++ include/maidsafe/nfs/types.h
@@ -17,6 +17,7 @@
 #include <ostream>
 #include <string>
 
+#include "maidsafe/data_types/structured_data_versions.h"
 #include "maidsafe/data_types/data_name_variant.h"
 #include "maidsafe/common/node_id.h"
 #include "maidsafe/common/types.h"
@@ -163,26 +164,26 @@ std::basic_ostream<Elem, Traits>& operator<<(std::basic_ostream<Elem, Traits>& o
 template <Persona PersonaType>
 struct PersonaTypes {
   typedef DataNameVariant DbKey;
+//  typedef NonEmptyString SerialisedDbKey;
+  typedef NonEmptyString DbValue;
   struct UnresolvedEntryKey {
     DbKey db_key;
     MessageAction action;
   };
-
  static const Persona persona = PersonaType;
 };
 
 template<>
 struct PersonaTypes<Persona::kStructuredDataManager> {
   typedef std::pair<DataNameVariant, Identity> DbKey;
-
+//  typedef NonEmptyString SerialisedDbKey;
+  typedef StructuredDataVersions DbValue;
   struct UnresolvedEntryKey {
     DbKey db_key;
     MessageAction action;
   };
-
  static const Persona persona = Persona::kStructuredDataManager;
 };
-
 
 typedef TaggedValue<Identity, struct MessageIdTag> MessageId;
 
