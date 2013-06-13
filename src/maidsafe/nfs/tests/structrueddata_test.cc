474a4c8533d0193f8843bad21c6420f82321751b
maqi <qi.ma@maidsafe.net>
2013-06-13 08:24:01 +0100

    extend constructor & serialise tests for StructuredData

--- src/maidsafe/nfs/tests/structrueddata_test.cc
+++ src/maidsafe/nfs/tests/structrueddata_test.cc
@@ -40,6 +40,21 @@ TEST_F(StructuredDataTest, BEH_Constructor) {
 
   StructuredData structured_data_ori(versions);
   StructuredData structured_data_copy(structured_data_ori);
+
+  uint32_t num_of_versions(RandomUint32() % 256 + 10);
+  for(uint32_t i(0); i < num_of_versions; ++i) {
+    StructuredDataVersions::VersionName version_name(
+        RandomUint32(), ImmutableData::name_type(Identity(RandomString(64))));
+    versions.push_back(version_name);
+  }
+
+  StructuredData structured_data(versions);
+  auto versions_fetched(structured_data.versions());
+  EXPECT_EQ(num_of_versions + 1, versions_fetched.size());
+  for(uint32_t i(0); i < (num_of_versions + 1); ++i) {
+    EXPECT_EQ(versions[i].id, versions_fetched[i].id);
+    EXPECT_EQ(versions[i].index, versions_fetched[i].index);
+  }
 }
 
 TEST_F(StructuredDataTest, BEH_Serialise) {
@@ -54,12 +69,21 @@ TEST_F(StructuredDataTest, BEH_Serialise) {
   }
 
   std::vector<StructuredDataVersions::VersionName> versions;
-  StructuredDataVersions::VersionName version_name(
-      RandomUint32(), ImmutableData::name_type(Identity(RandomString(64))));
-  versions.push_back(version_name);
+  uint32_t num_of_versions(RandomUint32() % 256 + 10);
+  for(uint32_t i(0); i < num_of_versions; ++i) {
+    StructuredDataVersions::VersionName version_name(
+        RandomUint32(), ImmutableData::name_type(Identity(RandomString(64))));
+    versions.push_back(version_name);
+  }
 
   StructuredData structured_data_ori(versions);
   StructuredData structured_data_serialise(structured_data_ori.Serialise());
+  auto versions_parsed(structured_data_serialise.versions());
+  EXPECT_EQ(num_of_versions, versions_parsed.size());
+  for(uint32_t i(0); i < num_of_versions; ++i) {
+    EXPECT_EQ(versions[i].id, versions_parsed[i].id);
+    EXPECT_EQ(versions[i].index, versions_parsed[i].index);
+  }
 }
 
 }  // namespace test
