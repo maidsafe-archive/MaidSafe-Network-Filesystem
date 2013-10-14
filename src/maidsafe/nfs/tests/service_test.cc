24924a2eb8e3e060a1b1b8eb537b34640fa5e948
Qi Ma <qi.ma@maidsafe.net>
2013-10-14 11:19:12 +0100

    change service_test to typed test

--- src/maidsafe/nfs/tests/service_test.cc
+++ src/maidsafe/nfs/tests/service_test.cc
@@ -40,62 +40,35 @@ namespace maidsafe {
 namespace nfs {
 
 namespace test {
-/*
-TEST(MaidNodeService, DISABLED_BEH_All) {
-  passport::Anmaid anmaid;
-  passport::Maid maid(anmaid);
-  routing::Routing routing(maid);
-  AsioService asio_service(2);
-  routing::Timer<nfs_client::MaidNodeService::GetResponse::Contents> get_timer(asio_service);
-  routing::Timer<nfs_client::MaidNodeService::GetVersionsResponse::Contents> get_versions_timer(
-      asio_service);
-  routing::Timer<nfs_client::MaidNodeService::GetBranchResponse::Contents> get_branch_timer(
-      asio_service);
-  maidsafe::nfs::Service<nfs_client::MaidNodeService> service(
-      std::move(std::unique_ptr<nfs_client::MaidNodeService>(new nfs_client::MaidNodeService(
-          routing, get_timer, get_versions_timer, get_branch_timer))));
 
-  ImmutableData immutable_data(NonEmptyString(RandomString(10)));
-  nfs_client::DataNameAndContentOrReturnCode contents(immutable_data);
+template <typename T>
+class ServiceTest : public ::testing::Test {
+};
 
-  typedef nfs::GetResponseFromDataManagerToMaidNode GetResponse;
-  GetResponse get_response(contents);
-  auto serialised_get_response(get_response.Serialise());
+typedef ::testing::Types<nfs_client::MaidNodeService, nfs_client::DataGetterService> ServiceTypes;
+TYPED_TEST_CASE(ServiceTest, ServiceTypes);
 
-  NodeId sender_node_id(NodeId::kRandomId);
-  NodeId sender_group_id(NodeId::kRandomId);
-  NodeId receiver_node_id(routing.kNodeId());
-  GetResponse::Sender sender((routing::GroupId(sender_node_id)),
-                             (routing::SingleId(sender_group_id)));
-  GetResponse::Receiver receiver(receiver_node_id);
-
-  auto response_tuple(ParseMessageWrapper(serialised_get_response));
-
-  service.HandleMessage(response_tuple, sender, receiver);
-}
-*/
-
-TEST(MaidNodeServiceTest, BEH_All) {
+TYPED_TEST(ServiceTest, MaidNodeServiceTest_BEH_All) {
   passport::Anmaid anmaid;
   passport::Maid maid(anmaid);
   routing::Routing routing(maid);
   AsioService asio_service(2);
   asio_service.Start();
-  typedef nfs_client::MaidNodeService::GetResponse GetResponse;
-  routing::Timer<GetResponse::Contents> get_timer(asio_service);
-  routing::Timer<nfs_client::MaidNodeService::GetVersionsResponse::Contents> get_versions_timer(
+  typedef typename TypeParam::GetResponse GetResponse;
+  routing::Timer<typename GetResponse::Contents> get_timer(asio_service);
+  routing::Timer<typename TypeParam::GetVersionsResponse::Contents> get_versions_timer(
       asio_service);
-  routing::Timer<nfs_client::MaidNodeService::GetBranchResponse::Contents> get_branch_timer(
+  routing::Timer<typename TypeParam::GetBranchResponse::Contents> get_branch_timer(
       asio_service);
-  Service<nfs_client::MaidNodeService> service(
-      std::move(std::unique_ptr<nfs_client::MaidNodeService>(new nfs_client::MaidNodeService(
+  Service<TypeParam> service(
+      std::move(std::unique_ptr<TypeParam>(new TypeParam(
           routing, get_timer, get_versions_timer, get_branch_timer))));
 
   ImmutableData immutable_data(NonEmptyString(RandomString(10)));
 
   // Set timer callback
   auto task_id(get_timer.NewTaskId());
-  auto callback([immutable_data](GetResponse::Contents returned_contents) {
+  auto callback([immutable_data](typename GetResponse::Contents returned_contents) {
       ImmutableData retrieved(ImmutableData::Name(returned_contents.data->name.raw_name),
                               ImmutableData::serialised_type(returned_contents.data->content));
       EXPECT_EQ(immutable_data.name(), retrieved.name())
@@ -105,16 +78,16 @@ TEST(MaidNodeServiceTest, BEH_All) {
   });
   get_timer.AddTask(std::chrono::milliseconds(500), callback, 1, task_id);
 
-  GetResponse::Contents contents(immutable_data);
+  typename GetResponse::Contents contents(immutable_data);
   GetResponse get_response(MessageId(task_id), contents);
   auto serialised_get_response(get_response.Serialise());
 
   NodeId sender_node_id(NodeId::kRandomId);
   NodeId sender_group_id(NodeId::kRandomId);
   NodeId receiver_node_id(routing.kNodeId());
-  GetResponse::Sender sender((routing::GroupId(sender_node_id)),
-                             (routing::SingleId(sender_group_id)));
-  GetResponse::Receiver receiver(receiver_node_id);
+  typename GetResponse::Sender sender((routing::GroupId(sender_node_id)),
+                                      (routing::SingleId(sender_group_id)));
+  typename GetResponse::Receiver receiver(receiver_node_id);
 
   auto response_tuple(ParseMessageWrapper(serialised_get_response));
 
