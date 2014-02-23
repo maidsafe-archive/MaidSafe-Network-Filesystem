27a3a7910377cd2d8959fc3946a3369e4735ca27
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2014-02-23 23:11:57 +0000

    Updated to match new sub-module names.

--- src/maidsafe/nfs/client/messages.cc
+++ src/maidsafe/nfs/client/messages.cc
@@ -44,8 +44,8 @@ maidsafe_error GetError(int error_value, const std::string& error_category_name)
     return MakeError(static_cast<DriveErrors>(error_value));
   if (error_category_name == std::string(GetVaultCategory().name()))
     return MakeError(static_cast<VaultErrors>(error_value));
-  if (error_category_name == std::string(GetClientCategory().name()))
-    return MakeError(static_cast<ClientErrors>(error_value));
+  if (error_category_name == std::string(GetApiCategory().name()))
+    return MakeError(static_cast<ApiErrors>(error_value));
 
   BOOST_THROW_EXCEPTION(MakeError(CommonErrors::parsing_error));
 }
